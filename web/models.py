756c13aa4677402c73ac37da365fe6afe2dc08ee
kelrogg <61114591+Kelrogg@users.noreply.github.com>
2022-12-06 02:16:46 +0300

    added: db models

--- web/models.py
+++ web/models.py
@@ -9,13 +9,106 @@ from docx import Document
 from docx.shared import Inches
 
 
-class Diagnosis(models.Model):
-    name = models.CharField(max_length=200, null=False)
-    description = models.TextField(null=True, blank=True)
-    text_to_onclusion = models.TextField(null=True, blank=True)
+class Article(models.Model):
+    number = models.PositiveSmallIntegerField()
 
     def __str__(self):
-        return self.name
+        return self.number
+
+class Record(models.Model):
+    date = models.DateTimeField()
+    visited = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.date
+
+class CorrectionalWork(models.Model):
+    type = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.type
+
+class Admin(models.Model):
+    login = models.CharField(max_length=45)
+    password = models.CharField(max_length=100)
+    create_time = models.DateTimeField()
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    rank = models.CharField()
+    gender = models.PositiveSmallIntegerField()
+    birthday = models.DateField()
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
+class User(models.Model):
+    login = models.CharField(max_length=45)
+    password = models.CharField(max_length=100)
+    create_time = models.DateTimeField()
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    gender = models.PositiveSmallIntegerField()
+    birthday = models.DateField()
+    admin_id = models.ForeignKey(
+        Admin,
+        on_delete = models.CASCADE
+    )
+    records = models.ManyToManyField(
+        Record
+    )
+    articles = models.ManyToManyField(
+        Article
+    )
+    correctional_works = models.ManyToManyField(
+        CorrectionalWork,
+        through='UserHasCorrectionalWork'
+    )
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
+class Meeting(models.Model):
+    meetingcol = models.CharField(max_length=45)
+    date = models.DateTimeField()
+    theme = models.CharField(max_length=100)
+    admin_id = models.ForeignKey(
+        Admin,
+        on_delete = models.CASCADE
+    )
+    user_id = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE
+    )
+
+    def __str__(self):
+        return self.theme
+
+class UserHasCorrectionalWork():
+    date = models.DateField()
+    user_id = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE
+    )
+    correctional_work_id = models.ForeignKey(
+        Record,
+        on_delete = models.CASCADE
+    )
+    admin_id = models.ForeignKey(
+        Admin,
+        on_delete = models.CASCADE
+    )
+
+
+
+
+
+
+
+
+
+
+# TODO Delete to hell
 
 
 class TemporaryContraindications(models.Model):
@@ -124,66 +217,6 @@ class Treatment(models.Model):
         if self.temporary_contraindications:
             return [i.name for i in self.temporary_contraindications.all()]
         return []
-
-    def formalize_to_document(self, file_name) -> str:
-        self.document = Document()
-        self.document.add_heading('Заключение', 0)
-
-        p1 = self.document.add_paragraph('')
-        p1.add_run("Пациент: ").bold = True
-        p1.add_run(self.patient.full_name)
-
-        p2 = self.document.add_paragraph('')
-        p2.add_run("Дата рождения: ").bold = True
-
-        p3 = self.document.add_paragraph('')
-        p3.add_run("Возраст: ").bold = True
-        p3.add_run(str(self.patient.age) + " лет")
-
-        p4 = self.document.add_paragraph('')
-        p4.add_run("Дата консультации: ").bold = True
-        now = datetime.datetime.now()
-        self.date = str(now.strftime("%d-%m-%Y %H:%M")).split()[0]
-        p4.add_run(self.date)
-
-        p5 = self.document.add_paragraph('')
-        p5.add_run("Анамнез: ").bold = True
-
-        if self.patient.diagnoses.all():
-            self.document.add_paragraph("Сопутствующие патологии: ")
-            for i in self.patient.diagnoses.all():
-                self.document.add_paragraph(i.name, style='List Bullet')
-
-        if self.temporary_contraindications.all():
-            self.document.add_paragraph("Противопоказания: ")
-            for i in self.temporary_contraindications.all():
-                self.document.add_paragraph(i, style='List Bullet')
-
-        p6 = self.document.add_paragraph('')
-        p6.add_run("Данные обследования: ").bold = True
-        self.document.add_paragraph("Уровень сознания: " + str(self.conscious_level))
-        self.document.add_paragraph("Неврологический дефицит: " + str(self.neurological_deficit))
-        if self.has_stroke_symptoms:
-            self.document.add_paragraph("Присутствуют симптомы инсульта")
-        else:
-            self.document.add_paragraph("Симптомы инсульта отсутствуют")
-        if self.is_injury:
-            self.document.add_paragraph("Была получена травма")
-
-        p7 = self.document.add_paragraph('')
-        p7.add_run("Диагноз: ").bold = True
-        p7.add_run(decode_label_detail(ClassificationType.objects.filter(prediction=self.predict).all()))
-
-        p8 = self.document.add_paragraph('')
-        p8.add_run("Рекомендовано: ").bold = True
-
-        if self.predict.recommend_text.treatment_tactics:
-            for recommend in self.predict.recommend_text.treatment_tactics.split('.')[:-1]:
-                self.document.add_paragraph(recommend + '.', style='List Bullet')
-
-        path = f'documents/{file_name}.docx'
-        self.document.save(path)
-        return path
 
 
 class FAQ(models.Model):
--- web/urls.py
+++ web/urls.py
@@ -7,19 +7,20 @@ from .views_test import treatment_form_short_view
 from . import views
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
+    path('', include('django.contrib.auth.urls'), name='home'),
     path('cabinet/', views.cabinet_view, name='cabinet'),
-    url(r'^profile/(?P<pk>\d+)$', views.DoctorProfileDetailView.as_view(), name='profile'),
-    path('peoples/', views.DoctorsListView.as_view(), name='peoples'),
     path('treatment-form', views.treatment_form_view, name='treatment_form'),
     path('treatment-form-short', treatment_form_short_view),
+    path('signup/', views.sign_up, name='sign_up'),
+    path('FAQ/', views.reference_view, name='reference'),
+
+    url(r'^profile/(?P<pk>\d+)$', views.DoctorProfileDetailView.as_view(), name='profile'),
+    path('peoples/', views.DoctorsListView.as_view(), name='peoples'),
     url(r'^report/(?P<pk>\d+)$', views.TreatmentDetailView.as_view(), name='treatment_report'),
     path('treatment-list/', views.TreatmentListView.as_view(), name='treatment_list'),
     path('history', views.TreatmentHistoryView.as_view(), name='history_list'),
     url(r'^treatment/(?P<pk>\d+)$', views.TreatmentDetailView.as_view(), name='treatment_detail'),
     url('api/login', views.api_login),
-    path('signup/', views.sign_up, name='sign_up'),
-    path('FAQ/', views.reference_view, name='reference'),
     path('refit_knn', views.refit_knn, name='refit_knn'),
     url('formalize_document/(?P<pk>\d+)$', views.formalize_treatment_to_document, name='formalize_document'),
 ]
