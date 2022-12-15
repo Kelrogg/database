8e4f3ea4ce73de659b05f2fb15c0ec6e06f89342
Alexandr Prohorov <efimpetr02@gmail.com>
2022-12-15 16:46:47 +0300

    registration form

--- web/forms.py
+++ web/forms.py
@@ -21,93 +21,70 @@ def choice_to_bool(string: str) -> bool:
 
 
 class TreatmentForm(forms.Form):
-    unknown_text = 'Неизвестно'
-    unknown_placeholder = get_placeholder_widget(unknown_text)
+    first_name = forms.CharField(max_length=45, required=False, label='Имя',
+                                widget=forms.TextInput(attrs={'placeholder': ''}))
+    last_name = forms.CharField(max_length=45, required=False, label='Фамилия',
+                                widget=forms.TextInput(attrs={'placeholder': ''}))
+    birthday = forms.DateField(label='Дата рождения', input_formats=[
+    '%Y-%m-%d',  # '2006-10-25'
+    '%m/%d/%Y',  # '10/25/2006'
+    '%m/%d/%y',  # '10/25/06'
+    '%b %d %Y',  # 'Oct 25 2006'
+    '%b %d, %Y',  # 'Oct 25, 2006'
+    '%d %b %Y',  # '25 Oct 2006'
+    '%d %b, %Y',  # '25 Oct, 2006'
+    '%B %d %Y',  # 'October 25 2006'
+    '%B %d, %Y',  # 'October 25, 2006'
+    '%d %B %Y',  # '25 October 2006'
+    '%d %B, %Y',  # '25 October, 2006'
+    ], 
+    widget=forms.TextInput(attrs={'placeholder': ''}))
 
-    full_name = forms.CharField(max_length=100, required=False, label='ФИО пациента',
-                                widget=unknown_placeholder)
-    age = forms.IntegerField(max_value=100, min_value=1, required=False,
-                             label='Возраст', widget=get_placeholder_widget(unknown_text, 'num'))
-
-    time_passed = forms.IntegerField(min_value=0, required=False,
-                                     label="Время начала симптоматики (ч)", widget=unknown_placeholder)
-    hematoma_volume = forms.IntegerField(max_value=200, required=False,
-                                         label="Объем гематомы (см³)",
-                                         widget=get_placeholder_widget(unknown_text, 'num-2'))
-
-    is_injure = forms.ChoiceField(
-        choices=[(True, 'Да'), (False, 'Нет'), (None, 'Неизвестно')],
+    article = forms.CharField(max_length= 45, required=False, label='Действующая статья АК РФ:',
+                               widget=forms.TextInput(attrs={'placeholder': ''}))
+    gender = forms.ChoiceField(
+        choices=[(True, 'Муж'), (True, 'Жен')],
         initial=None,
-        label='Травма',
+        label='Пол',
         required=False,
         widget=forms.RadioSelect,
     )
+    login = forms.CharField(max_length=45, required=False, label='Логин',
+                                widget=forms.TextInput(attrs={'placeholder': ''}))
+    password = forms.CharField(max_length=45, required=False, label='Пароль',
+                                widget=forms.TextInput(attrs={'placeholder': ''}))
+                                    
 
-    has_stroke_symptoms = forms.ChoiceField(required=False,
-                                            label='Симптомы инусльта',
-                                            choices=[(True, 'Да'), (False, 'Нет')],
-                                            initial=True,
-                                            widget=forms.RadioSelect)
 
-    neurological_deficit = forms.ChoiceField(
-        choices=[(1, 'I'), (2, 'II'), (3, 'III'), (4, 'IV')],
-        widget=forms.RadioSelect,
-        initial=1,
-        label='Неврологический дефицит*'
-    )
 
-    conscious_level = forms.ChoiceField(
-        choices=[(15, '15 Ясное'), (14, '14-13 Умеренное оглушение'), (12, '12-11 Глубокое оглушение'),
-                 (9, '10-8 Сопор'), (7, 'Умеренная кома'), (5, 'Глубокая кома'), (3, 'Терминальная кома')],
-        initial=15,
-        widget=forms.RadioSelect,
-        label='Неврологический дефицит*'
-    )
 
-    diagnoses = forms.ModelMultipleChoiceField(
-        queryset=Diagnosis.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        label="Сопутствующие паталогии",
-        required=False,
-    )
 
-    temporary_contraindications = forms.ModelMultipleChoiceField(
-        queryset=TemporaryContraindications.objects.all(),
-        widget=forms.CheckboxSelectMultiple,
-        label="Иные противопоказания",
-        required=False,
-    )
 
-    snapshot = forms.ImageField(required=True)
-
-    def clean_snapshot(self):
-        data = self.cleaned_data['snapshot']
-        return data
-
+# 
 
 class PersonalData(forms.Form):
-    full_name = forms.CharField(max_length=100, required=True, label='ФИО')
-    qualification = forms.CharField(max_length=100, required=False, label='Квалификация')
-    experience = forms.IntegerField(min_value=0, max_value=70, required=False, label='Стаж')
-    work_place = forms.CharField(max_length=100, required=False, label='Место работы')
-    education = forms.CharField(max_length=100, required=False, label='Образование')
-    contacts = forms.CharField(max_length=100, required=False, label='Контакты')
-    image = forms.ImageField(required=False, allow_empty_file=False)
+     full_name = forms.CharField(max_length=100, required=True, label='ФИО')
+     qualification = forms.CharField(max_length=100, required=False, label='Квалификация')
+     experience = forms.IntegerField(min_value=0, max_value=70, required=False, label='Стаж')
+     work_place = forms.CharField(max_length=100, required=False, label='Место работы')
+     education = forms.CharField(max_length=100, required=False, label='Образование')
+     contacts = forms.CharField(max_length=100, required=False, label='Контакты')
+     image = forms.ImageField(required=False, allow_empty_file=False)
 
 
 class SignUpForm(UserCreationForm):
-    full_name = forms.CharField(max_length=100, required=True)
-    email = forms.EmailField(max_length=254, required=True)
-    password1 = forms.CharField(widget=forms.PasswordInput())
-    qualification = forms.CharField(max_length=40, required=False)
-    work_place = forms.CharField(max_length=60, required=False)
-    education = forms.CharField(max_length=30, required=False)
-    experience = forms.IntegerField(min_value=0, required=False)
-    contacts = forms.CharField(max_length=150, required=False)
-    photo = forms.ImageField(required=True)
+     full_name = forms.CharField(max_length=100, required=True)
+     email = forms.EmailField(max_length=254, required=True)
+     password1 = forms.CharField(widget=forms.PasswordInput())
+     qualification = forms.CharField(max_length=40, required=False)
+     work_place = forms.CharField(max_length=60, required=False)
+     education = forms.CharField(max_length=30, required=False)
+     experience = forms.IntegerField(min_value=0, required=False)
+     contacts = forms.CharField(max_length=150, required=False)
+     photo = forms.ImageField(required=True)
 
-    class Meta:
-        model = User
-        fields = ('full_name', 'email', 'password1', 'qualification', 'work_place',
-                  'education', 'experience', 'contacts', 'photo')
+     class Meta:
+         model = User
+         fields = ('full_name', 'email', 'password1', 'qualification', 'work_place',
+                   'education', 'experience', 'contacts', 'photo')
 
--- web/models.py
+++ web/models.py
@@ -1,3 +1,248 @@
+# import datetime
+# from django.contrib.auth.models import User
+# from django.db import models
+# from django.urls import reverse
+# from django.utils.html import mark_safe
+# from django.conf import settings
+# from .LabelDecoder import decode_label_detail
+# from docx import Document
+# from docx.shared import Inches
+
+
+
+# class Article(models.Model):
+#     number = models.PositiveSmallIntegerField()
+
+#     def __str__(self):
+#         return self.number
+
+# class Record(models.Model):
+#     date = models.DateTimeField()
+#     visited = models.PositiveSmallIntegerField()
+
+#     def __str__(self):
+#         return self.date
+
+# class CorrectionalWork(models.Model):
+#     type = models.CharField(max_length=100)
+#     address = models.CharField(max_length=100)
+
+#     def __str__(self):
+#         return self.type
+
+# class Admin(models.Model):
+#     login = models.CharField(max_length=45)
+#     password = models.CharField(max_length=100)
+#     create_time = models.DateTimeField()
+#     first_name = models.CharField(max_length=45)
+#     last_name = models.CharField(max_length=45)
+#     rank = models.CharField()
+#     gender = models.PositiveSmallIntegerField()
+#     birthday = models.DateField()
+
+#     def __str__(self):
+#         return self.first_name + self.last_name
+
+# class User(models.Model):
+#     login = models.CharField(max_length=45)
+#     password = models.CharField(max_length=100)
+#     create_time = models.DateTimeField()
+#     first_name = models.CharField(max_length=45)
+#     last_name = models.CharField(max_length=45)
+#     gender = models.PositiveSmallIntegerField()
+#     birthday = models.DateField()
+#     admin_id = models.ForeignKey(
+#         Admin,
+#         on_delete = models.CASCADE
+#     )
+#     records = models.ManyToManyField(
+#         Record
+#     )
+#     articles = models.ManyToManyField(
+#         Article
+#     )
+#     correctional_works = models.ManyToManyField(
+#         CorrectionalWork,
+#         through='UserHasCorrectionalWork'
+#     )
+
+#     def __str__(self):
+#         return self.first_name + self.last_name
+
+# class Meeting(models.Model):
+#     meetingcol = models.CharField(max_length=45)
+#     date = models.DateTimeField()
+#     theme = models.CharField(max_length=100)
+#     admin_id = models.ForeignKey(
+#         Admin,
+#         on_delete = models.CASCADE
+#     )
+#     user_id = models.ForeignKey(
+#         User,
+#         on_delete = models.CASCADE
+#     )
+
+#     def __str__(self):
+#         return self.theme
+
+# class UserHasCorrectionalWork():
+#     date = models.DateField()
+#     user_id = models.ForeignKey(
+#         User,
+#         on_delete = models.CASCADE
+#     )
+#     correctional_work_id = models.ForeignKey(
+#         Record,
+#         on_delete = models.CASCADE
+#     )
+#     admin_id = models.ForeignKey(
+#         Admin,
+#         on_delete = models.CASCADE
+#     )
+
+
+
+
+
+
+
+
+
+
+# # TODO Delete to hell
+
+
+# class Diagnosis(models.Model):
+#     name = models.CharField(max_length=200, null=False)
+#     description = models.TextField(null=True, blank=True)
+#     text_to_onclusion = models.TextField(null=True, blank=True)
+
+#     def __str__(self):
+#         return self.name
+
+# class TemporaryContraindications(models.Model):
+#     name = models.CharField(max_length=200, null=False)
+#     description = models.TextField(null=True, blank=True)
+#     text_to_onclusion = models.TextField(null=True, blank=True)
+
+#     def __str__(self):
+#         return self.name
+
+
+# class Patient(models.Model):
+#     full_name = models.CharField(max_length=100, null=True, blank=True)
+#     age = models.PositiveSmallIntegerField(null=True, blank=True)
+
+#     diagnoses = models.ManyToManyField(Diagnosis, blank=True)
+
+#     def __str__(self):
+#         return self.full_name
+
+#     def get_diagnoses_name_list(self):
+#         if self.diagnoses:
+#             return [i.name for i in self.diagnoses.all()]
+#         return []
+
+
+# class Doctor(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+#     full_name = models.CharField(max_length=100, null=False, blank=False, default='Фамилия имя отчество')
+#     qualification = models.CharField(max_length=100, null=True, blank=True)
+#     experience = models.PositiveSmallIntegerField(null=True, blank=True)
+#     work_place = models.CharField(max_length=100, null=True, blank=True)
+#     education = models.CharField(max_length=100, null=True, blank=True)
+#     contacts = models.CharField(max_length=100, null=True, blank=True)
+#     photo = models.ImageField(null=True, default='profile_photo.jpg')
+
+#     def __str__(self):
+#         return f'{self.full_name} {self.user.username}'
+
+
+# class RecommendText(models.Model):
+#     operation = models.CharField(max_length=100)
+#     treatment_tactics = models.TextField()
+#     tactics_if_agree = models.TextField()
+
+
+# class NeuronetPrediction(models.Model):
+#     confidence = models.FloatField(null=True)
+
+#     recommend_text = models.OneToOneField(RecommendText, on_delete=models.CASCADE, null=True)
+
+#     def __str__(self):
+#         return f'{decode_label_detail(self.classification_types)} : {format(self.confidence, ".2f")}%'
+
+
+# class ClassificationType(models.Model):
+#     value = models.PositiveSmallIntegerField()
+#     prediction = models.ForeignKey(NeuronetPrediction, on_delete=models.CASCADE,
+#                                    related_name='classification_types')
+
+
+# def rename_file_by_pk(instance, filename):
+#     ext = filename.split('.')[-1]
+#     return 'snapshots/{}.{}'.format(Treatment.objects.count() + 1, ext)
+
+
+# class Treatment(models.Model):
+#     neurological_deficit = models.PositiveSmallIntegerField(null=False, default=1)
+#     conscious_level = models.PositiveSmallIntegerField(null=False, default=15)
+
+#     time_passed = models.PositiveSmallIntegerField(null=True, blank=True)
+#     hematoma_volume = models.PositiveSmallIntegerField(null=True, blank=True)
+
+#     is_injury = models.BooleanField(default=False, null=True, blank=True)
+#     has_stroke_symptoms = models.BooleanField(default=False, null=False)
+
+#     temporary_contraindications = models.ManyToManyField(TemporaryContraindications, blank=True)
+
+#     snapshot = models.ImageField(upload_to=rename_file_by_pk, null=True, blank=True)
+#     patient = models.ForeignKey(Patient, on_delete=models.CASCADE, null=True, blank=True)
+#     doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, null=True, default=None, blank=True)
+
+#     predict = models.OneToOneField(NeuronetPrediction,
+#                                    on_delete=models.CASCADE, null=True, blank=True)
+
+#     def __str__(self):
+#         patology = ''
+#         if self.predict:
+#             patology = decode_label_detail(ClassificationType.objects.filter(prediction=self.predict).all())
+#         return f'{self.patient.full_name}.{patology} '
+
+#     def get_snapshot_html(self):
+#         return mark_safe(f'<img src="{settings.MEDIA_URL}{self.snapshot}" alt="File image"/>')
+
+#     def get_snapshot_for_admin(self):
+#         HEIGHT = 120
+#         WIDTH = 200
+#         return mark_safe(f'<img src="{settings.MEDIA_URL}{self.snapshot}" '
+#                          f'style=width:{WIDTH}px; height:{HEIGHT}px; alt="File image"/>')
+
+#     def get_absolute_url(self):
+#         return reverse('treatment_detail', args=[str(self.id)])
+
+#     def get_temporary_contraindications_name_list(self):
+#         if self.temporary_contraindications:
+#             return [i.name for i in self.temporary_contraindications.all()]
+#         return []
+
+
+# class FAQ(models.Model):
+#     name = models.CharField(max_length=100, null=True, blank=True)
+
+#     def __str__(self):
+#         return self.name
+
+
+# class FAQItem(models.Model):
+#     header = models.CharField(max_length=100, null=False)
+#     text = models.TextField(null=False)
+#     reference = models.ForeignKey(FAQ, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.header
+
 import datetime
 from django.contrib.auth.models import User
 from django.db import models
@@ -9,106 +254,13 @@ from docx import Document
 from docx.shared import Inches
 
 
-class Article(models.Model):
-    number = models.PositiveSmallIntegerField()
+class Diagnosis(models.Model):
+    name = models.CharField(max_length=200, null=False)
+    description = models.TextField(null=True, blank=True)
+    text_to_onclusion = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return self.number
-
-class Record(models.Model):
-    date = models.DateTimeField()
-    visited = models.PositiveSmallIntegerField()
-
-    def __str__(self):
-        return self.date
-
-class CorrectionalWork(models.Model):
-    type = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.type
-
-class Admin(models.Model):
-    login = models.CharField(max_length=45)
-    password = models.CharField(max_length=100)
-    create_time = models.DateTimeField()
-    first_name = models.CharField(max_length=45)
-    last_name = models.CharField(max_length=45)
-    rank = models.CharField()
-    gender = models.PositiveSmallIntegerField()
-    birthday = models.DateField()
-
-    def __str__(self):
-        return self.first_name + self.last_name
-
-class User(models.Model):
-    login = models.CharField(max_length=45)
-    password = models.CharField(max_length=100)
-    create_time = models.DateTimeField()
-    first_name = models.CharField(max_length=45)
-    last_name = models.CharField(max_length=45)
-    gender = models.PositiveSmallIntegerField()
-    birthday = models.DateField()
-    admin_id = models.ForeignKey(
-        Admin,
-        on_delete = models.CASCADE
-    )
-    records = models.ManyToManyField(
-        Record
-    )
-    articles = models.ManyToManyField(
-        Article
-    )
-    correctional_works = models.ManyToManyField(
-        CorrectionalWork,
-        through='UserHasCorrectionalWork'
-    )
-
-    def __str__(self):
-        return self.first_name + self.last_name
-
-class Meeting(models.Model):
-    meetingcol = models.CharField(max_length=45)
-    date = models.DateTimeField()
-    theme = models.CharField(max_length=100)
-    admin_id = models.ForeignKey(
-        Admin,
-        on_delete = models.CASCADE
-    )
-    user_id = models.ForeignKey(
-        User,
-        on_delete = models.CASCADE
-    )
-
-    def __str__(self):
-        return self.theme
-
-class UserHasCorrectionalWork():
-    date = models.DateField()
-    user_id = models.ForeignKey(
-        User,
-        on_delete = models.CASCADE
-    )
-    correctional_work_id = models.ForeignKey(
-        Record,
-        on_delete = models.CASCADE
-    )
-    admin_id = models.ForeignKey(
-        Admin,
-        on_delete = models.CASCADE
-    )
-
-
-
-
-
-
-
-
-
-
-# TODO Delete to hell
+        return self.name
 
 
 class TemporaryContraindications(models.Model):
@@ -217,6 +369,66 @@ class Treatment(models.Model):
         if self.temporary_contraindications:
             return [i.name for i in self.temporary_contraindications.all()]
         return []
+
+    def formalize_to_document(self, file_name) -> str:
+        self.document = Document()
+        self.document.add_heading('Заключение', 0)
+
+        p1 = self.document.add_paragraph('')
+        p1.add_run("Пациент: ").bold = True
+        p1.add_run(self.patient.full_name)
+
+        p2 = self.document.add_paragraph('')
+        p2.add_run("Дата рождения: ").bold = True
+
+        p3 = self.document.add_paragraph('')
+        p3.add_run("Возраст: ").bold = True
+        p3.add_run(str(self.patient.age) + " лет")
+
+        p4 = self.document.add_paragraph('')
+        p4.add_run("Дата консультации: ").bold = True
+        now = datetime.datetime.now()
+        self.date = str(now.strftime("%d-%m-%Y %H:%M")).split()[0]
+        p4.add_run(self.date)
+
+        p5 = self.document.add_paragraph('')
+        p5.add_run("Анамнез: ").bold = True
+
+        if self.patient.diagnoses.all():
+            self.document.add_paragraph("Сопутствующие патологии: ")
+            for i in self.patient.diagnoses.all():
+                self.document.add_paragraph(i.name, style='List Bullet')
+
+        if self.temporary_contraindications.all():
+            self.document.add_paragraph("Противопоказания: ")
+            for i in self.temporary_contraindications.all():
+                self.document.add_paragraph(i, style='List Bullet')
+
+        p6 = self.document.add_paragraph('')
+        p6.add_run("Данные обследования: ").bold = True
+        self.document.add_paragraph("Уровень сознания: " + str(self.conscious_level))
+        self.document.add_paragraph("Неврологический дефицит: " + str(self.neurological_deficit))
+        if self.has_stroke_symptoms:
+            self.document.add_paragraph("Присутствуют симптомы инсульта")
+        else:
+            self.document.add_paragraph("Симптомы инсульта отсутствуют")
+        if self.is_injury:
+            self.document.add_paragraph("Была получена травма")
+
+        p7 = self.document.add_paragraph('')
+        p7.add_run("Диагноз: ").bold = True
+        p7.add_run(decode_label_detail(ClassificationType.objects.filter(prediction=self.predict).all()))
+
+        p8 = self.document.add_paragraph('')
+        p8.add_run("Рекомендовано: ").bold = True
+
+        if self.predict.recommend_text.treatment_tactics:
+            for recommend in self.predict.recommend_text.treatment_tactics.split('.')[:-1]:
+                self.document.add_paragraph(recommend + '.', style='List Bullet')
+
+        path = f'documents/{file_name}.docx'
+        self.document.save(path)
+        return path
 
 
 class FAQ(models.Model):
