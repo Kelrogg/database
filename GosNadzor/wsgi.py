9006ca6b36b610d61534fbf5aaef31febaf4657d
Alexandr Prohorov <efimpetr02@gmail.com>
2023-02-19 14:38:14 +0300

    some upgrades

--- GosNadzor/wsgi.py
+++ GosNadzor/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DrCAT.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GosNadzor.settings')
 
 application = get_wsgi_application()
