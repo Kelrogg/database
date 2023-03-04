76a0b8cd327d36d7d8ebefc2cd8c34fe141513e8
Alexandr Prohorov <efimpetr02@gmail.com>
2023-03-04 13:03:31 +0300

    calenders beauty

--- calendarapp/forms.py
+++ calendarapp/forms.py
@@ -10,12 +10,12 @@ class EventForm(ModelForm):
         # datetime-local is a HTML5 input type
         widgets = {
             "title": forms.TextInput(
-                attrs={"class": "form-control", "placeholder": "Enter event title"}
+                attrs={"class": "form-control", "placeholder": "Введите название события"}
             ),
             "description": forms.Textarea(
                 attrs={
                     "class": "form-control",
-                    "placeholder": "Enter event description",
+                    "placeholder": "Введите описание события",
                 }
             ),
             "start_time": DateInput(
