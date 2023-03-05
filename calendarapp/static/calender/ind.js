ec4c64cadbf790e94f811ee9b7c9a96bc40a40e9
Alexandr Prohorov <efimpetr02@gmail.com>
2023-03-05 16:16:27 +0300

    styles

---
+++ calendarapp/static/calender/ind.js
@@ -0,0 +1,6 @@
+const namea = document.querySelector(".btn btn-info right mr-2");
+const btn = document.querySelector("a");
+
+      btn.addEventListener("click", function () {
+        namea.style.color = "blue";
+ });
